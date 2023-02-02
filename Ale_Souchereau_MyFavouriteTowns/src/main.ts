87aea59cbcc3b49df70311131955bf8a5c064c1e
alexsouchereau <W0632604@myscc.ca>
2023-02-02 17:34:33 -0500

    Imported zone js for stackblitz

--- Ale_Souchereau_MyFavouriteTowns/src/main.ts
+++ Ale_Souchereau_MyFavouriteTowns/src/main.ts
@@ -2,6 +2,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+// Required by stackblitz
+import 'zone.js';
+import 'zone.js/dist/long-stack-trace-zone.js';
+
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
