c8daf22979e84a3b0abfd5a432cde29c9db52349
alexsouchereau <W0632604@myscc.ca>
2023-02-15 18:40:43 -0500

    Fixed pipe to include content with null or undefined type

--- Ale_Souchereau_MyFavouriteTowns/src/app/filter-content.pipe.ts
+++ Ale_Souchereau_MyFavouriteTowns/src/app/filter-content.pipe.ts
@@ -1,4 +1,4 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import {Pipe, PipeTransform} from '@angular/core';
 import {Content} from "./helper-files/content-interface";
 
 @Pipe({
@@ -6,9 +6,14 @@ import {Content} from "./helper-files/content-interface";
 })
 export class FilterContentPipe implements PipeTransform {
 
-  transform(contentList: Content[], searchTerm?: string) {
-    let filteredList : Content[] = contentList.filter(c => c.type === searchTerm);
-    return filteredList;
+  transform(contentList: Content[], filter?: string) {
+    if (contentList) {
+      if (filter) {
+        return contentList.filter(c => c.type === filter);
+      } else {
+        return contentList.filter(c => (c.type === '' || c.type === null || c.type === undefined))
+      }
+    } else return;
   }
 
 }
