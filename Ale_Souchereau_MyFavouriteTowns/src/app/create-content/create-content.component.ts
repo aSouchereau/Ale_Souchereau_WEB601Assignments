cff5f279caea8646b0819f747a97ed6b7877baf9
alexsouchereau <W0632604@myscc.ca>
2023-03-02 16:30:45 -0500

    Created submit handler

--- Ale_Souchereau_MyFavouriteTowns/src/app/create-content/create-content.component.ts
+++ Ale_Souchereau_MyFavouriteTowns/src/app/create-content/create-content.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-create-content',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class CreateContentComponent {
 
+
+  submitHandler(formData: NgForm) {
+    console.log(formData.value);
+    console.log(formData.valid);
+  }
 }
