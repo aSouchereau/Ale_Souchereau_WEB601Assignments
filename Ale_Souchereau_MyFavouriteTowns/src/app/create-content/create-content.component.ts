b8123863eca3dacf077ae4632d5c3fa4dbadf37a
alexsouchereau <W0632604@myscc.ca>
2023-03-02 23:38:53 -0500

    Added promise and validation

--- Ale_Souchereau_MyFavouriteTowns/src/app/create-content/create-content.component.ts
+++ Ale_Souchereau_MyFavouriteTowns/src/app/create-content/create-content.component.ts
@@ -22,8 +22,33 @@ export class CreateContentComponent {
 
 
   submitHandler() {
-    if (this.newTownItem.tags)
-      this.newTownItem.tags = this.newTownItem.tags.toString().split(',');
-    this.newTownEvent.emit(this.newTownItem);
+    this.emitNewItem()
+      .then((success) => {
+        console.log(success);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  emitNewItem() {
+    return new Promise((success, fail) => {
+      if (this.newTownItem.id && this.newTownItem.title && this.newTownItem.description && this.newTownItem.creator) {
+        // If new item has tags, split into array
+        if (this.newTownItem.tags)
+          this.newTownItem.tags = this.newTownItem.tags.toString().split(',');
+
+        this.newTownEvent.emit(this.newTownItem);
+
+        let tempBool = true; // Temporary solution for now, cannot figure out how to check if emit function worked
+        if (tempBool) {
+          success("New item added successfully");
+        } else {
+          fail("Could not add new item.");
+        }
+      } else {
+        fail("Please fill in all required fields");
+      }
+    });
   }
 }
